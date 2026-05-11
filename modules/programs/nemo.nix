403f89bd5817be7fa67a8547178c665bdf2b91b3
tarzoq <119268536+Tarzoq@users.noreply.github.com>
2026-05-11 22:18:36 +0200

    Finally figured how to combine NixOS system configuration and home-manager configurations in the same file. In this case defining Nemo as my standard file manager, but also including Thunar as a system package to allow me to use it's Bulk Renamer utility via Nemo when renaming multiple files.
    Former-commit-id: bda59d05d296cba887fead6bb491fbebb907545f

--- modules/programs/nemo.nix
+++ modules/programs/nemo.nix
@@ -3,6 +3,25 @@
 {
   environment.systemPackages = with pkgs; [
     nemo-with-extensions
+    thunar #for bulk rename
   ];
   services.gvfs.enable = true;
+
+  #make it the default file browser
+  xdg = {
+    mime.defaultApplications = {
+      "inode/directory" = [ "nemo.desktop" ];
+      "application/x-gnome-saved-search" = [ "nemo.desktop" ];
+    };
+  };
+
+  home-manager.users."${vars.user.name}" = {
+    dconf = {
+      settings = {
+          "org/cinnamon/desktop/applications/terminal" = {
+              exec = "kitty";
+          };
+      };
+    };
+  };
 }
