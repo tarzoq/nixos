8042ce2fd08e5286b5c72b5fce5104470ba8c819
tarzoq <119268536+Tarzoq@users.noreply.github.com>
2026-06-08 21:59:47 +0200

    Make Noctalia bar transparent
    Former-commit-id: 0247289f7f259aef1806f413673977e4be8e3289

--- modules/vicinae.nix
+++ modules/vicinae.nix
@@ -4,6 +4,9 @@
   #  vicinae
   #];
 
+  #dynamic theming: 
+  #https://docs.vicinae.com/theming/matugen
+
   home-manager.users."${vars.user.name}" = {
     programs.vicinae = {
       enable = true;
