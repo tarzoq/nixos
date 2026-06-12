0915b9943532e2f240743c3d7ef12be4544ed825
tarzoq <119268536+Tarzoq@users.noreply.github.com>
2026-06-13 00:21:16 +0200

    Concluded that the Vicinae Browser Extension doesn't work yet, at least without spending more time on it.
    Former-commit-id: 8e89053d36d2845b9d34fc62a5b03528a656a35c

--- modules/vicinae.nix
+++ modules/vicinae.nix
@@ -1,16 +1,11 @@
 { config, pkgs, vars, ... }:
 {
-  #environment.systemPackages = with pkgs; [
-  #  vicinae
-  #];
-
   #dynamic theming: 
   #https://docs.vicinae.com/theming/matugen
 
   home-manager.users."${vars.user.name}" = {
     programs.vicinae = {
       enable = true;
-      #systemd.enable = true;
     };
   };
 }
