7d09c6ba9bf2078b63b95cfad53409f53512b729
tarzoq <119268536+Tarzoq@users.noreply.github.com>
2026-06-14 23:09:33 +0200

    Disable http2 for Steam through home-manager activation script to fix slow download speeds.
    Add fanatac kernel driver for gaming.
    
    
    Former-commit-id: c70d55fbdf730fe0dc7914b6a94d2c03b2f62da7

--- modules/programs/gaming.nix
+++ modules/programs/gaming.nix
@@ -6,11 +6,29 @@
   #gamemoderun %command%
   #mangohud %command%
 
+  hardware.hid-fanatecff.enable = true; #kernel driver with force feedback support for fanatec
+
   programs.steam = {
     enable = true;
     gamescopeSession.enable = true; #starts game in optimized compositor
     extest.enable = true; #make mouse input from controller work for wayland
   };
+  ########## STEAM disable http2 #######################
+  home-manager.users."${vars.user.name}" = { lib, ... }: {
+    home.activation.steamConfig = lib.hm.dag.entryAfter ["writeBoundary"] ''
+      cfg="$HOME/.steam/steam/steam_dev.cfg"
+      key="@nClientDownloadEnableHTTP2PlatformLinux"
+      setting="$key 0"
+
+      if [ ! -f "$cfg" ]; then
+        mkdir -p "$(dirname "$cfg")"
+	echo "$setting" > "$cfg"
+      elif ! grep -qF "$setting" "$cfg"; then
+        echo "$setting" >> "$cfg"
+      fi
+    '';
+  };
+  ########## STEAM disable http2 #######################
 
   programs.gamemode.enable = true; #daemon that improves game performance
   users.users.${vars.user.name}.extraGroups = [ "gamemode" ];
