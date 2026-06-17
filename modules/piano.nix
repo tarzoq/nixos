6ed19aa8f56451616ab0a97c28bfc7e938ffb3ff
tarzoq <119268536+Tarzoq@users.noreply.github.com>
2026-06-17 22:51:05 +0200

    Done with first draft of "Piano Toggle"-script. It essentially allows me to easily go from normal laptop use to rotate screen, enable idleInhibitor and start SheetMusicViewer with one keybind. Afterwards I then press the same keybind, to have everything go back to the way it was before I started.
    Former-commit-id: b8b28b455e69144bce666aa27ee273b93d3a3144

--- modules/piano.nix
+++ modules/piano.nix
@@ -4,13 +4,40 @@
     ./programs/sheetmusicviewer.nix
   ];
 
-  #modules.niri.imports = "include \"piano.hm.kdl\"";
+  #programs.ydotool.enable = true;
+  #users.users.${vars.user.name}.extraGroups = [ "ydotool" ];
 
-  #home-manager.users."${vars.user.name}" = {
-  #  home.file."nixos/config/niri/piano.hm.kdl".text = ''
-  #    bind {
-  #      Super+Mod5+A hotkey-overlay-title="Mode Toggle: Piano" { spawn-sh ""; }
-  #    }
-  #  '';
-  #};
+  modules.niri.imports = "include \"piano.hm.kdl\"";
+
+  home-manager.users."${vars.user.name}" = {
+    home.file."nixos/config/niri/piano.hm.kdl".text = ''
+      binds {
+        Super+Mod5+1 hotkey-overlay-title="Mode Toggle: Piano" { 
+          spawn-sh "
+            currentTransform=$(niri msg --json focused-output | jq .logical.transform)
+            
+            if [ $currentTransform = '\"Normal\"' ]; then
+              //rotate screen 180°
+              niri msg output $(niri msg --json focused-output | jq -r '.name') transform 270
+
+              //keep awake on
+              noctalia-shell ipc call idleInhibitor enable
+
+              //start sheetmusicviewer
+	      sheet-music-viewer
+            else
+              //rotate screen normal
+              niri msg output $(niri msg --json focused-output | jq -r '.name') transform normal
+
+              //keep awake off
+              noctalia-shell ipc call idleInhibitor disable
+
+	      //kill sheetmusicviewer
+	      pkill -f sheet-music-viewer
+            fi
+          "; 
+        }
+      }
+    '';
+  };
 }
