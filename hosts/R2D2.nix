163fe506247a28460f875f894da969711baabe66
tarzoq <119268536+Tarzoq@users.noreply.github.com>
2026-06-19 17:28:57 +0200

    Suspend problem progress: modprobe before suspend didn't change anything. I think the root problem is modern standby. And since my T14s doesn't support S3 there's not much left for me to try.
    Former-commit-id: e02096e101a03c171b9599a5306a945020f850ec

--- hosts/R2D2.nix
+++ hosts/R2D2.nix
@@ -16,21 +16,21 @@
   modules.hibernate.resumeOffset = 4278272;
   modules.hibernate.diskUuid = "253b6ad1-b80b-4441-9081-4e2f54a17782";
   modules.hibernate.ramGb = 32;
-  #modules.hibernate.hibernateScript = ''
-  #  /run/current-system/sw/bin/modprobe -r ath11k_pci;
-  #'';
-  #modules.hibernate.resumeScript = ''
-  #  /run/current-system/sw/bin/modprobe ath11k_pci;
-  #'';
-
-  modules.resume.downScript = ''
+  modules.hibernate.hibernateScript = ''
     /run/current-system/sw/bin/modprobe -r ath11k_pci;
-    /run/current-system/sw/bin/systemctl stop NetworkManager
   '';
-  modules.resume.resumeScript = ''
+  modules.hibernate.resumeScript = ''
     /run/current-system/sw/bin/modprobe ath11k_pci;
-    /run/current-system/sw/bin/systemctl restart NetworkManager
   '';
+
+  #modules.resume.downScript = ''
+  #  /run/current-system/sw/bin/modprobe -r ath11k_pci;
+  #  /run/current-system/sw/bin/systemctl stop NetworkManager
+  #'';
+  #modules.resume.resumeScript = ''
+  #  /run/current-system/sw/bin/modprobe ath11k_pci;
+  #  /run/current-system/sw/bin/systemctl restart NetworkManager
+  #'';
 
   ##fix weird Lenovo Trackpoint issue (even present on Windows)
   #modules.resume.resumeScript = ''
