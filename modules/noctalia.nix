a3900b484a2a3c44c73e45ac595020fc683c815a
tarzoq <119268536+Tarzoq@users.noreply.github.com>
2026-08-04 03:07:10 +0200

    Add Phone Connect widget
    Make vicinae also restart with special keybind
    
    
    Former-commit-id: 3a60efae4c15ddcd3e1ee7d52532e8a709d64c7b

--- modules/noctalia.nix
+++ modules/noctalia.nix
@@ -5,9 +5,11 @@
   environment.systemPackages = with pkgs; [
     inputs.noctalia.packages.${pkgs.stdenv.hostPlatform.system}.default
     ddcutil #desktop monitor brightness control
-    pulseaudio
+    pulseaudio #audio switcher
     #qt6.qtwebsockets #hass plugin
     #wtype #clipper
+    glib #phone connect
+    sshfs #phone connect
     ];
 
   #add support for calendar events
@@ -27,8 +29,9 @@
       systemd.enable = true;
     };
 
-    home.file."nixos/config/niri/noctalia-restart.hm.sh".text = ''
+    home.file."nixos/config/niri/noctalia-and-vicinae-restart.hm.sh".text = ''
       /run/current-system/sw/bin/systemctl --user restart noctalia.service
+      /run/current-system/sw/bin/systemctl --user restart vicinae.service
     '';
   };
 }
